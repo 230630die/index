b7f3deb4069526db9ca6ba3866c2a249bb3d5fef
RyuKyoungHyun <soccergh@naver.com>
2023-07-05 17:59:04 +0900

    12

--- index.js
+++ index.js
@@ -3,6 +3,8 @@
 const news = document.querySelector("main .first-page-container .text-wrapper");
 const fp = document.querySelector("main .first-page-container");
 const fph = document.querySelector("main .first-page-container").clientHeight;
+const sp = document.querySelector("main .second-page-container");
+const tp = document.querySelector("main .third-page-container");
 const scrolled = () => {
   const fpsh = fph - scrollY;
   if(scrollY > 1) {
@@ -12,10 +14,41 @@ const scrolled = () => {
     $('nav').removeClass('active');
     fp.style.height = 92 +'vh';
   }
+};
+const newscroll = () => {
   if(scrollY > 100) {
     news.style.transform = `translate(-50% , ${scrollY-100}%)`;
   } else {
     news.style.transform = `translate(-50%, 0)`
   }
-};
-addEventListener("scroll", scrolled);
+}
+if (scrollY < 1200) {
+  addEventListener('scroll',newscroll);
+} else {
+
+}
+// if (scrollY < sp.offsetTop){
+//   addEventListener("scroll", scrolled);
+// } else {
+//   removeEventListener ('scroll',scrolled);
+// }
+addEventListener("scroll",scrolled);
+
+const circle = document.querySelector('main .third-page-container .circle-wrapper');
+const scc = circle.scroll({behavior:"smooth"})
+function cs1() {
+  const cs = () => {
+    console.log(scrollY-circle.offsetTop);
+    if (scrollY > circle.offsetTop) {
+      circle.style.width = scrollY + 'px';
+    } else {
+      circle.style.width = 0 + 'px';
+    }
+  }
+  addEventListener('scroll',cs);
+}
+setInterval(cs1, 1000);
+
+// document.addEventListener("scroll",(e) => {
+//   document.querySelector('main .third-page-container .circle-wrapper')
+// })
