a4dba441ba072bb504d382a403d437d6056439e3
RyuKyoungHyun <soccergh@naver.com>
2023-07-05 11:27:45 +0900

    1234

--- index.js
+++ index.js
@@ -1,14 +1,14 @@
 
 // 첫페이지 움직임 코드
-const news = document.querySelector(".news");
-const fp = document.querySelector("main .first-page");
-const fph = document.querySelector("main .first-page").clientHeight;
+const news = document.querySelector("main .first-page-container .first-page-container-txt-wrapper");
+const fp = document.querySelector("main .first-page-container");
+const fph = document.querySelector("main .first-page-container").clientHeight;
 const scrolled = () => {
   const fpsh = fph - scrollY;
-  const nwsh = fpsh - fph - scrollY;
-  fp.style.height = 300 + fpsh + 'px';
-  if(scrollY > 300) {
-    news.style.transform = `translate(-50% , ${-nwsh - 700}px)`;
+  fp.style.height = fpsh + 'px';
+  if(scrollY > 100) {
+    // news.style.bottom = -scrollY + 100 + 'px';
+    news.style.transform = `translate(-50% , ${scrollY-100}%)`;
   }else{
     fp.style.height = 90+'vh';
     news.style.transform = `translate(-50%, 0)`
